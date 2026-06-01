760b317892f54747f8312668abc4dd892e74f76f
saniya mehra <mehrasaniya868@gmail.com>
2026-06-01 22:06:35 +0530

    organize app.jsx code

--- emergency-backend/controllers/emergencyController.js
+++ emergency-backend/controllers/emergencyController.js
@@ -1,28 +1,40 @@
-let emergencies =[];
-exports.addEmergency = (req,res) => {
-    emergencies.push(req.body);
-    res.send("Emergency Added ✅");
+const Emergency = require("../models/Emergency");
+exports.addEmergency = async(req,res) => {
+    try {
+        const newEmergency =new Emergency(req.body);
+
+        await newEmergency.save();
+        res.send("Emergency Added ✅");
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 };
     exports.getEmergencies = (req,res) => {
-        res.json(emergencies);
-    };
-
-    exports.updateEmergency = (req,res) => {
-        const index =req.params.index;
-        if(emergencies[index]){
-            emergencies[index]=req.body;
-            res.send("Emergency Updated✏️");
-        }else{
-            res.send("Data not found❌ ");
+        try{
+            const data =await Emergency.find();
+            res.json(data);
+        } catch (error) {
+             res.status(500).send(error.message);
         }
     };
-    exports.deleteEmergency = (req,res) => {
-        const index = req.params.index;
-        if(emergencies [index]){
-            emergencies.splice(index,1);
-            res.send("Emergency Deleted🗑️");
 
-        }else{
-            res.send("Not Found ❌");
+    exports.updateEmergency = async(req,res) => {
+        try {
+            const id = req.params.id;
+            await Emergency.findByIdAndUpdate(id, req.body);
+            res.send("Emergency Updated✏️");
+
+        }catch (error) {
+             res.status(500).send(error.message);
+        }
+    };
+        
+    exports.deleteEmergency = (req,res) => {
+        try {
+         const id =req.params.id;
+            await Emergency.findByIdAndDelete(id);
+            res.send("Emergency Deleted🗑️");
+        } catch (error) {
+            res.status(500).send(error.message);
         }
     };
--- emergency-backend/server.js
+++ emergency-backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
-
+mongoose.connect("mongodb+srv://admin:admin123@cluster0.i8tzba4.mongodb.net/emergencyDB?retryWrites=true&w=majority&appName=Cluster0")
+.then(()=> console.log("MongoDB connected successfully"))
+.catch((err) => console.log(err));
 const app = express();
 
 app.use (cors());
@@ -12,7 +15,6 @@ app.use("/",emergencyRoutes);
 app.get("/",(req,res)=>{
     res.send("Emergency Backend Running 🚀");
 });
-
 app.listen(5000,()=>{
     console.log("Server is running on port 5000");
 });
