a961435cd276aacc88750108247b91bb77d68a08
saniya mehra <mehrasaniya868@gmail.com>
2026-06-21 00:06:58 +0530

    enhance contact.js and css with better ui

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,26 +1,47 @@
 import "./Contacts.css";
 const Contacts = () => {
+  const copyNumbers =(number) =>{
+    navigator.clipboard.writeText(number);
+    alert(`${number} copied`);
+  }
   return (
     <div className="contacts-page">
       <div className="page-card">
         <h2>Emergency Contacts</h2>
         <p>Quick access to important emergency helplines.</p>
         <div className="contact-actions">
-  <a href="tel:112" className="action-btn action-btn--red">
-    🚨 Call Emergency
+ <div className="action-btn action-btn--red">
+  <h3>🚨 Emergency</h3>
+  <h2>112</h2>
+  <a href="tel:112" className="call-now-btn">
+    📞 Call Now
   </a>
+</div>
 
-  <a href="tel:100" className="action-btn action-btn--blue">
-    👮 Call Police
+ <div className="action-btn action-btn--blue">
+  <h3>👮 Police</h3>
+  <h2>100</h2>
+  <a href="tel:100" className="call-now-btn">
+    📞 Call Now
   </a>
+</div>
 
-  <a href="tel:101" className="action-btn action-btn--amber">
-    🔥 Call Fire Brigade
+  <div className="action-btn action-btn--amber">
+  <h3>🔥 Fire Brigade</h3>
+  <h2>101</h2>
+  <a href="tel:101" className="call-now-btn">
+    📞 Call Now
   </a>
+</div>
 
-  <a href="tel:102" className="action-btn action-btn--green">
-    🚑 Call Ambulance
+<div className="action-btn action-btn--green">
+  <h3>🚑 Ambulance</h3>
+  <h2>102</h2>
+
+  <a href="tel:102" className="call-now-btn">
+    📞 Call Now
   </a>
+</div>
   </div>
     <div className="important-contacts">
   <h3>Important Contacts</h3>
@@ -28,34 +49,113 @@ const Contacts = () => {
    Other important helpline numbers
   </p>
   <div className="contact-list">
+
   <a href="tel:112" className="contact-item">
       <span>🚨 National Emergency</span>
-      <strong>112</strong>
+      <div className="contact-actions-row">
+    <strong>112</strong>
+      <button
+      className="copy-btn"
+      onClick={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        copyNumbers("112");
+      }}
+    >
+      📋
+    </button>
+    </div>
     </a>
 
-     <a href="tel:100" className="contact-item">
-      <span>👮 Police</span>
-      <strong>100</strong>
-    </a>
+<a href="tel:100" className="contact-item">
+  <span>👮 Police</span>
 
-    <a href="tel:101" className="contact-item">
-      <span>🔥 Fire Brigade</span>
-      <strong>101</strong>
-    </a>
+  <div className="contact-actions-row">
+    <strong>100</strong>
+    <button
+      className="copy-btn"
+      onClick={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        copyNumbers("100");
+      }}
+    >
+      📋
+    </button>
+  </div>
+</a>
 
-    <a href="tel:102" className="contact-item">
-      <span>🚑 Ambulance</span>
-      <strong>102</strong>
-    </a>
-    <a href="tel:1091" className="contact-item">
-      <span>📞 Women Helpline</span>
-      <strong>1091</strong>
-    </a>
+<a href="tel:101" className="contact-item">
+  <span>🔥 Fire Brigade</span>
 
-    <a href="tel:1098" className="contact-item">
-      <span>🧒 Child Helpline</span>
-      <strong>1098</strong>
-    </a>
+  <div className="contact-actions-row">
+    <strong>101</strong>
+    <button
+      className="copy-btn"
+      onClick={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        copyNumbers("101");
+      }}
+    >
+      📋
+    </button>
+  </div>
+</a>
+
+<a href="tel:102" className="contact-item">
+  <span>🚑 Ambulance</span>
+
+  <div className="contact-actions-row">
+    <strong>102</strong>
+    <button
+      className="copy-btn"
+      onClick={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        copyNumbers("102");
+      }}
+    >
+      📋
+    </button>
+  </div>
+</a>
+
+<a href="tel:1091" className="contact-item">
+  <span>📞 Women Helpline</span>
+
+  <div className="contact-actions-row">
+    <strong>1091</strong>
+    <button
+      className="copy-btn"
+      onClick={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        copyNumbers("1091");
+      }}
+    >
+      📋
+    </button>
+  </div>
+</a>
+
+<a href="tel:1098" className="contact-item">
+  <span>🧒 Child Helpline</span>
+
+  <div className="contact-actions-row">
+    <strong>1098</strong>
+    <button
+      className="copy-btn"
+      onClick={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        copyNumbers("1098");
+      }}
+    >
+      📋
+    </button>
+  </div>
+</a>
   </div>
 </div>
      <div className="emergency-guide">
